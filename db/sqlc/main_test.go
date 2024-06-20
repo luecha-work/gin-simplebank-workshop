403f88e63f9700445797d70b07ab57d3dc7425ed
luecha-work <luecha.kanm@gmail.com>
2024-06-20 22:29:15 +0700

    Added store testing

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -11,19 +11,21 @@ import (
 
 const (
 	dbDriver = "postgres"
-	dbSourc  = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
 )
 
 var testQueries *Queries
+var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	conn, err := sql.Open(dbDriver, dbSourc)
+	var err error
 
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
 
-	testQueries = New(conn)
+	testQueries = New(testDB)
 
 	os.Exit(m.Run())
 }
