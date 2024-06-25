f7bb123345064d97b1aee0b8b036151c9c672e6e
luecha-work <luecha.kanm@gmail.com>
2024-06-25 22:45:37 +0700

    adding api

---
+++ api/server.go
@@ -0,0 +1,22 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	db "github.com/techschool/simple_bank/db/sqlc"
+)
+
+// Server serves HTTP requests for our banking service.
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts", server.CreateAccount())
+
+	server.router = router
+	return server
+}
