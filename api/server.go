165dc969e8da91800475bfd76b7b665bd210de8a
luecha.kan <luecha.kanm@gmail.com>
2024-07-02 22:20:57 +0700

    Added create CreateAccount api

--- api/server.go
+++ api/server.go
@@ -15,8 +15,18 @@ func NewServer(store *db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
-	// router.POST("/accounts", server.CreateAccount())
+	router.POST("/accounts", server.CreateAccount)
 
 	server.router = router
 	return server
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{
+		"error": err.Error(),
+	}
 }
--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+	"github.com/techschool/simple_bank/api"
+	db "github.com/techschool/simple_bank/db/sqlc"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("cannot start server: ", err)
+	}
+
 }
