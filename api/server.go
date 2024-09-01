02b2d940ff536534e7f96a0f2d3fe464fe045b84
luecha-work <luecha.kanm@gmail.com>
2024-09-01 17:12:42 +0700

    Added refresh token api

--- api/server.go
+++ api/server.go
@@ -44,6 +44,7 @@ func (server *Server) setupRouter() {
 
 	router.POST("/users", server.createUser)
 	router.POST("/users/login", server.loginUser)
+	router.POST("/token/refresh-token", server.renewAccessToken)
 
 	//TODO: Use Middleware for routes
 	authRoutes := router.Group("/").Use(authMiddleware(server.tokenMaker))
