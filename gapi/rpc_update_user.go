0679d884f58d57f8e855e990228324ad011ecd31
luecha-work <luecha.kanm@gmail.com>
2024-10-06 16:03:01 +0700

    Add authorization to protect gRPC API

--- gapi/rpc_update_user.go
+++ gapi/rpc_update_user.go
@@ -16,9 +16,18 @@ import (
 
 func (server *Server) UpdateUser(ctx context.Context, req *pb.UpdateUserRequest) (*pb.UpdateUserResponse, error) {
 	//TODO: Add authorization
+	authPayload, err := server.authorizeUser(ctx, []string{utils.BankerRole, utils.DepositorRole})
+	if err != nil {
+		return nil, unauthenticatedError(err)
+	}
+
 	violations := validateUpdateUserRequest(req)
 	if violations != nil {
 		return nil, invalidArgumentError(violations)
+	}
+
+	if authPayload.Role != utils.BankerRole && authPayload.Username != req.GetUsername() {
+		return nil, status.Errorf(codes.PermissionDenied, "cannot update other user's info")
 	}
 
 	arg := db.UpdateUserParams{
