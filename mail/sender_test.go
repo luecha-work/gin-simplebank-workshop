d67f651b939a16a430cc4cf13cfca642ca84c4f6
luecha.kan <luecha.kanm@gmail.com>
2024-12-05 17:21:51 +0700

    send email in Go via Gmail and added unit testing

---
+++ mail/sender_test.go
@@ -0,0 +1,27 @@
+package mail
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/techschool/simple_bank/utils"
+)
+
+func TestSendEmailWithGmail(t *testing.T) {
+	config, err := utils.LoadConfig("..")
+	require.NoError(t, err)
+
+	sender := NewGmailSender(config.EmailSenderName, config.EmailSenderAddress, config.EmailSenderPassword)
+
+	subject := "A test email"
+	content := `
+	<h1>Hello Gin Framework</h1>
+	<p>This is a test email sent using <b>Gin Framework</b>.</p>
+	`
+	to := []string{"luecha.kanm@gmail.com"}
+	attachFiles := []string{"../README.md"}
+
+	err = sender.SendMail(subject, content, to, nil, nil, attachFiles)
+
+	require.NoError(t, err)
+}
