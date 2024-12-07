4fc3739d269d6ff0a4930e6b97e6b68782ee82a0
luecha.kan <luecha.kanm@gmail.com>
2024-12-07 17:10:15 +0700

    Test a gRPC API that requires authentication

--- gapi/main_test.go
+++ gapi/main_test.go
@@ -1,13 +1,17 @@
 package gapi
 
 import (
+	"context"
+	"fmt"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
 	db "github.com/techschool/simple_bank/db/sqlc"
+	"github.com/techschool/simple_bank/token"
 	"github.com/techschool/simple_bank/utils"
 	"github.com/techschool/simple_bank/worker"
+	"google.golang.org/grpc/metadata"
 )
 
 func newTestServer(t *testing.T, store db.Store, taskDistributor worker.TaskDistributor) *Server {
@@ -20,4 +24,18 @@ func newTestServer(t *testing.T, store db.Store, taskDistributor worker.TaskDist
 	require.NoError(t, err)
 
 	return server
+}
+
+func newContextWithBearerToken(t *testing.T, tokenMaker token.Maker, username string, role string, duration time.Duration) context.Context {
+	accessToken, _, err := tokenMaker.CreateToken(username, role, duration)
+	require.NoError(t, err)
+
+	bearerToken := fmt.Sprintf("%s %s", authorizationBearer, accessToken)
+	md := metadata.MD{
+		authorizationHeader: []string{
+			bearerToken,
+		},
+	}
+
+	return metadata.NewIncomingContext(context.Background(), md)
 }
